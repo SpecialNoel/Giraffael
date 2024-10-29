cb26ea1d4895889cbfa5b4cf622dd890beda61e3
JianmingLinUMass <jiaminglin@umass.edu>
2024-10-28 21:03:36 -0400

    updated Client.py and Server.py

--- Client.py
+++ Client.py
@@ -1,20 +1,47 @@
 import socket
+from datetime import datetime
+from threading import Thread
 
-def create_client_socket(serverAddress, serverPort):
+# python3 Client.py
+
+def create_client_socket(SERVER_HOST, SERVER_PORT):
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientSocket.connect((serverAddress, serverPort)) 
+    clientSocket.connect((SERVER_HOST, SERVER_PORT)) 
     return clientSocket
 
-def send(clientSocket, message):
-    clientSocket.send(message.encode())
-    print(f'Sent message: "{message}" to Server')
+def recv_msg_from_channel(clientSocket):
+    while True:
+        msg = clientSocket.recv(1024)
+        print('\n' + msg.decode())
+    
+def get_and_send_user_input_msg(clientSocket, clientName):
+    while True:
+        msg = input()
+        if msg.lower() == 'q':
+            print('\nDisconnected from the chatroom')
+            break
+        send_msg_to_channel(clientSocket, clientName, msg)
+
+def send_msg_to_channel(clientSocket, clientName, msg):
+    date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    msg = f'[{date_now} {clientName}:{msg}]'
+    clientSocket.send(msg.encode())
 
 if __name__ == '__main__':
-    serverAddress = ''
-    serverPort = 17990
-    clientSocket = create_client_socket(serverAddress, serverPort)
+    SERVER_HOST = '127.0.0.1'
+    SERVER_PORT = 5000
+    
+    clientSocket = create_client_socket(SERVER_HOST, SERVER_PORT)
+    clientName = input('Enter your username: ')
+    
+    t = Thread(target=recv_msg_from_channel, args=(clientSocket,))
+    t.daemon = True
+    t.start()
 
-    send(clientSocket, 'Hello, server')
+    print('\nType a message to send to the channel, or\nType q to disconnect')
+    t2 = Thread(target=get_and_send_user_input_msg, args=(clientSocket,clientName,))
+    t2.daemon = True
+    t2.start()
 
     clientSocket.close()
     print('Client socket closed')
--- Server.py
+++ Server.py
@@ -1,29 +1,56 @@
 import socket
+from threading import Thread
 
-def create_server_socket(serverAddress, serverPort):
+# python3 Server.py
+
+# Tutorial from https://thepythoncode.com/article/make-a-chat-room-application-in-python
+
+def create_server_socket(SERVER_HOST, SERVER_PORT):
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverSocket.bind((serverAddress, serverPort))
+    serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serverSocket.bind((SERVER_HOST, SERVER_PORT))
     return serverSocket
 
-def accept_connection(serverSocket):
+def handle_a_client(conn, address, clientSockets):
+    while True:
+        try:
+            msg = conn.recv(1024)
+        except Exception as e:
+            conn.close()
+            clientSockets.remove(conn)
+            print(f'Error: {e}. Removed {address} from client sockets')
+        else:
+            for socket in clientSockets:
+                socket.send(msg)
+
+def accept_a_connection(serverSocket, clientSockets):
     (conn, address) = serverSocket.accept()
     print(f'Accepted connection request from Client[{address}].')
     
-    with conn:
-        while True:
-            rcvpkt = conn.recv(1024)
-            if not rcvpkt: break
-            print(f'Received from Client[{address}]: ', rcvpkt.decode())
-    print(f'Disconnected from Client[{address}].')
+    clientSockets.add(conn)
     
+    # Start a new thread that handles this client only
+    t = Thread(target=handle_a_client, args=(conn,address,clientSockets,))
+    t.daemon = True # Make the thread daemon so it ends whenever the main thread ends
+    t.start()
+        
 if __name__=='__main__':
-    serverAddress = "127.0.0.1"
-    serverPort = 17990
-    serverSocket = create_server_socket(serverAddress, serverPort)
+    MAX_CLIENT_COUNT = 1
     
-    serverSocket.listen()
-    print(f'Server socket[{serverAddress}, {serverPort}] started listening.')
-    accept_connection(serverSocket)
+    SERVER_HOST = '127.0.0.1'
+    SERVER_PORT = 5000
+    serverSocket = create_server_socket(SERVER_HOST, SERVER_PORT)
+    
+    clientSockets = set()
+    
+    serverSocket.listen(MAX_CLIENT_COUNT)
+    print(f'Server socket[{SERVER_HOST}: {SERVER_PORT}] started listening. MAX {MAX_CLIENT_COUNT} Clients')
+    
+    while True:
+        accept_a_connection(serverSocket, clientSockets)
+        if not len(clientSockets): 
+            print('No more Client connected to the channel. Channel closed.')
+            break
     
     serverSocket.close()
     print('Server socket closed')
